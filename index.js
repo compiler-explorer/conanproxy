7da0b3e793993cf06517674efd5403c859409bab
Partouf <partouf@gmail.com>
2020-05-13 01:12:11 +0200

    api and html page added to test things

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const
     _ = require('underscore'),
-    fs = require('fs'),
+    fs = require('fs').promises,
     express = require('express'),
     httpProxy = require('http-proxy'),
     http = require('http'),
@@ -10,197 +10,204 @@ const
 const
     webServer = express();
 
-// const conanserverurl = 'http://127.0.0.1:9300';
-// const ceserverurl = 'https://godbolt.org'
+const conanserverurl = 'http://127.0.0.1:9300';
+const ceserverurl = 'https://godbolt.org'
 
-// let compilernames = null;
-// let allLibrariesAndVersions = null;
+const conanserverroot = "/home/partouf/.conan_server";
 
-// let availableLibrariesAndVersions = [];
+let compilernames = null;
+let allLibrariesAndVersions = null;
 
-// async function fetchCompilers() {
-// return new Promise((resolve, reject) => {
-//     https.get(`${ceserverurl}/api/compilers`, {headers: {'Accept': 'application/json'}}, (resp) => {
-//         let data = '';
-//         resp.on('data', (chunk) => data += chunk);
-//         resp.on('end', () => {
-//             const jsdata = JSON.parse(data);
-//             const compilers = {};
-//             _.each(jsdata, (obj) => {
-//                 compilers[obj.id] = obj.name;
-//             });
-//             compilernames = compilers
-//             resolve(true);
-//         })
-//     }).on('error', (err) => {
-//         console.error(err);
-//         reject(err);
-//     });
-// });
-// }
+let availableLibrariesAndVersions = {};
+let availableLibraryIds = [];
 
-// async function fetchAllLibraries() {
-// return new Promise((resolve, reject) => {
-//     https.get(`${ceserverurl}/api/libraries/c++`, {headers: {'Accept': 'application/json'}}, (resp) => {
-//         let data = '';
-//         resp.on('data', (chunk) => data += chunk);
-//         resp.on('end', () => {
-//             allLibrariesAndVersions = JSON.parse(data);
-//             resolve(true);
-//         })
-//     }).on('error', (err) => {
-//         console.error(err);
-//         reject(err);
-//     });
-// });
-// }
+async function getConanBinaries(library, version) {
+    return new Promise((resolve, reject) => {
+        const filteredlibrary = library.match(/([\w_-]*)/i)[1];
+        const filteredversion = version.match(/([\w0-9_\.]*)/i)[1];
+        if (filteredlibrary && filteredversion) {
+            const libandver = `${filteredlibrary}/${filteredversion}`;
 
-// async function fetchPackages(library, version) {
-// return new Promise((resolve, reject) => {
-//     const filteredlibrary = library.match(/([\w_-]*)/i)[1];
-//     const filteredversion = version.match(/([\w0-9_\.]*)/i)[1];
-//     if (filteredlibrary && filteredversion) {
-//         const libandver = `${filteredlibrary}/${filteredversion}`;
+            const url = `${conanserverurl}/v1/conans/${libandver}/${libandver}/search`;
+            console.log('calling ' + url);
+            http.get(url, (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => data += chunk);
+                resp.on('end', () => {
+                    let jsdata = null;
+                    try {
+                        jsdata = JSON.parse(data);
+                    } catch {
+                        resolve({});
+                        return;
+                    }
 
-//         http.get(`${conanserverurl}/v1/conans/${libandver}/${libandver}/search`, (resp) => {
-//             let data = '';
-//             resp.on('data', (chunk) => data += chunk);
-//             resp.on('end', () => {
-//                 const jsdata = JSON.parse(data);
-//                 const setPerCompiler = {};
-//                 const setOfCombinations = [];
-//                 _.each(jsdata, (obj) => {
-//                     const compilerid = obj.settings['compiler.version'];
-//                     const compilername = compilernames[compilerid];
-//                     const relevantSettings = _.omit(obj.settings, (val, key) => key.indexOf('compiler') === 0);
-//                     const settingsWithoutCompiler = _.map(
-//                         relevantSettings,
-//                         (val, key) => `${key}=${val}`).join(',');
+                    const setPerCompiler = {};
+                    const setOfCombinations = [];
+                    _.each(jsdata, (obj) => {
+                        const compilerid = obj.settings['compiler.version'];
+                        const compilername = compilernames[compilerid];
+                        const relevantSettings = _.omit(obj.settings, (val, key) => key.indexOf('compiler') === 0);
+                        const settingsWithoutCompiler = JSON.stringify(relevantSettings);
 
-//                     let idx = setOfCombinations.indexOf(settingsWithoutCompiler);
-//                     if (idx === -1) {
-//                         idx = setOfCombinations.length;
-//                         setOfCombinations.push(settingsWithoutCompiler);
-//                     }
+                        let idx = setOfCombinations.indexOf(settingsWithoutCompiler);
+                        if (idx === -1) {
+                            idx = setOfCombinations.length;
+                            setOfCombinations.push(settingsWithoutCompiler);
+                        }
 
-//                     if (!setPerCompiler[compilerid]) {
-//                         setPerCompiler[compilerid] = {
-//                             name: compilername,
-//                             combinations: []
-//                         };
-//                     }
+                        if (!setPerCompiler[compilerid]) {
+                            setPerCompiler[compilerid] = {
+                                name: compilername,
+                                combinations: []
+                            };
+                        }
 
-//                     setPerCompiler[compilerid].combinations.push(idx);
-//                 })
+                        setPerCompiler[compilerid].combinations.push(idx);
+                    })
 
-//                 resolve({
-//                     possibleCombinations: setOfCombinations,
-//                     perCompiler: setPerCompiler
-//                 });
-//             })
-//         }).on('error', (err) => {
-//             reject(err);
-//         });
-//     } else {
-//         reject('Not a valid library or version');
-//     }
-// });
-// }
+                    const orderedByCompilerId = {};
+                    Object.keys(setPerCompiler).sort().forEach((key) => {
+                        orderedByCompilerId[key] = setPerCompiler[key];
+                    });
 
-// async function fetchAndFilter() {
-// await fetchCompilers();
-// await fetchAllLibraries();
+                    resolve({
+                        possibleCombinations: _.map(setOfCombinations, (combo) => JSON.parse(combo)),
+                        perCompiler: orderedByCompilerId
+                    });
+                })
+            }).on('error', (err) => {
+                reject(err);
+            });
+        } else {
+            reject('Not a valid library or version');
+        }
+    });
+}
 
-// let librarypromises = _.map(allLibrariesAndVersions, (library) => {
-//     return new Promise((resolve) => {
-//         let versionpromises = _.map(library.versions, (version, verid) => {
-//             return fetchPackages(library.id, version.version)
-//                 .then((sets) => {
-//                     return {
-//                         libraryid: library.id,
-//                         libraryname: library.name,
-//                         verionid: verid,
-//                         version: version.version,
-//                         sets: sets
-//                     };
-//                 })
-//                 .catch(() => false);
-//         });
-
-//         Promise.all(versionpromises).then((all) => {
-//             resolve(_.filter(all, item => item !== false));
-//         });
-//     });
-// })
-
-// return Promise.all(librarypromises).then((libandver) => {
-//     return libandver;
-// });
-// }
-
-// fetchAndFilter().then((filteredLibraries) => {
-// availableLibrariesAndVersions = filteredLibraries;
-
-// console.log(availableLibrariesAndVersions);
-
-// webServer
-//     .get('/compilers', async (req, res) => {
-//         res.send(await fetchCompilers());
-//     })
-//     .get('/built_libraries', (req, res) => {
-//         res.send({libraries: {}})
-//     })
-//     .get('/binaries/:library/:version', async (req, res) => {
-//         const binaries = await fetchPackages(req.params.library, req.params.version);
-//         res.send(binaries);
-//     })
-//     .use((req, res, next) => {
-//         res.send({status: 404, message: `page "${req.path}" could not be found`});
-//     })
-//     .listen(10240);
-
-// });
-
-const proxy = httpProxy.createProxyServer({});
-proxy.on('proxyReq', function (proxyReq, req) {
-    if (!req.body || !Object.keys(req.body).length) {
-        return;
-    }
-
-    const contentType = proxyReq.getHeader('Content-Type');
-    let bodyData;
-
-    if (contentType === 'application/json') {
-        bodyData = JSON.stringify(req.body);
-    }
-
-    if (contentType === 'application/x-www-form-urlencoded') {
-        bodyData = req.body;
-    }
-
-    if (bodyData) {
-        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-        proxyReq.write(bodyData);
-    }
-});
-
-const conanRemote = {
-    target: "http://127.0.0.1:9300"
-};
-
-webServer
-    .get('/hello', async (req, res) => {
-        res.send('hello, world!');
-    })
-    .get('/libraries', async (req, res) => {
-        fs.dir("/home/ce/.conan_server").then((dirdata) => {
-            console.log(dirdata);
-            res.send('blabla');
+async function refreshCECompilers() {
+    return new Promise((resolve, reject) => {
+        https.get(`${ceserverurl}/api/compilers`, {headers: {'Accept': 'application/json'}}, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => data += chunk);
+            resp.on('end', () => {
+                const jsdata = JSON.parse(data);
+                const compilers = {};
+                _.each(jsdata, (obj) => {
+                    compilers[obj.id] = obj.name;
+                });
+                compilernames = compilers
+                resolve(true);
+            })
+        }).on('error', (err) => {
+            console.error(err);
+            reject(err);
         });
-    })
-    .use((req, res, next) => {
-        proxy.web(req, res, { target: conanRemote.target, changeOrigin: true }, e => {
-            next(e);
+    });
+}
+
+async function refreshCELibraries() {
+    return new Promise((resolve, reject) => {
+        https.get(`${ceserverurl}/api/libraries/c++`, {headers: {'Accept': 'application/json'}}, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => data += chunk);
+            resp.on('end', () => {
+                allLibrariesAndVersions = JSON.parse(data);
+                resolve(true);
+            })
+        }).on('error', (err) => {
+            console.error(err);
+            reject(err);
         });
+    });
+}
+
+function newProxy() {
+    const proxy = httpProxy.createProxyServer({});
+
+    proxy.on('proxyReq', function (proxyReq, req) {
+        if (!req.body || !Object.keys(req.body).length) {
+            return;
+        }
+
+        const contentType = proxyReq.getHeader('Content-Type');
+        let bodyData;
+
+        if (contentType === 'application/json') {
+            bodyData = JSON.stringify(req.body);
+        }
+
+        if (contentType === 'application/x-www-form-urlencoded') {
+            bodyData = req.body;
+        }
+
+        if (bodyData) {
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+            proxyReq.write(bodyData);
+        }
+    });
+
+    return proxy;
+}
+
+async function refreshConanLibraries() {
+    availableLibraryIds = [];
+    availableLibrariesAndVersions = {};
+
+    fs.opendir(`${conanserverroot}/data`).then(async (libraryDirs) => {
+        for await (const libraryDir of libraryDirs) {
+            const libraryId = libraryDir.name;
+            availableLibraryIds.push(libraryId);
+            const ceLib = _.find(allLibrariesAndVersions, (lib) => lib.id === libraryId);
+            if (ceLib) {
+                fs.opendir(`${conanserverroot}/data/${libraryId}`).then(async (versionDirs) => {
+                    for await (const versionDir of versionDirs) {
+                        const ceVersion = _.find(ceLib.versions, ver => ver.version === versionDir.name);
+                        if (ceVersion) {
+                            if (!availableLibrariesAndVersions[libraryId]) {
+                                availableLibrariesAndVersions[libraryId] = {
+                                    name: ceLib.name,
+                                    versions: {}
+                                };
+                            }
+                
+                            availableLibrariesAndVersions[libraryId].versions[ceVersion.id] = versionDir.name;
+                        }
+                    }
+                });
+            }
+        }
     })
-    .listen(10240);
+}
+
+function main() {
+    const proxy = newProxy();
+
+    webServer
+        .get('/hello', async (req, res) => {
+            res.send('hello, world!');
+        })
+        .get('/libraries', async (req, res) => {
+            res.send(availableLibrariesAndVersions);
+        })
+        .get('/binaries/:libraryid/:version', async (req, res) => {
+            const all = await getConanBinaries(req.params.libraryid, req.params.version);
+            res.send(all);
+        })
+        .use('/v1', (req, res, next) => {
+            req.url = `/v1${req.url}`;
+            proxy.web(req, res, { target: conanserverurl, changeOrigin: true }, e => {
+                next(e);
+            });
+        })
+        .use('/v2', (req, res, next) => {
+            req.url = `/v2${req.url}`;
+            proxy.web(req, res, { target: conanserverurl, changeOrigin: true }, e => {
+                next(e);
+            });
+        })
+        .use(express.static('html'))
+        .listen(10240);
+}
+
+refreshCECompilers().then(refreshCELibraries).then(refreshConanLibraries).then(main);
