d653899f20d5a17931258254ec7279eafc35daa2
Partouf <partouf@gmail.com>
2020-07-03 17:25:04 +0200

    sql-template-strings don't actually work it seems

--- build-logging.js
+++ build-logging.js
@@ -1,8 +1,7 @@
 
 const
     sqlite3 = require('sqlite3'),
-    { open } = require('sqlite'),
-    { SQL } = require('sql-template-strings');
+    { open } = require('sqlite');
 
 class BuildLogging {
     constructor(databasepath) {
@@ -15,7 +14,11 @@ class BuildLogging {
             filename: this.databasepath,
             driver: sqlite3.Database
           });
-          await this.connection.migrate();
+        // this.connection.on('trace', (data) => {
+        //     console.log(data);
+        // });
+
+        await this.connection.migrate();
     }
 
     getCurrentDateStr() {
@@ -25,25 +28,51 @@ class BuildLogging {
     }
 
     async setBuildFixed(library, library_version, compiler, compiler_version, arch, libcxx, compiler_flags) {
-        await this.connection.exec(
-            SQL`delete from latest
-                where library=${library}
-                  and library_version=${library_version}
-                  and compiler=${compiler}
-                  and compiler_version=${compiler_version}
-                  and arch=${arch}
-                  and libcxx=${libcxx}
-                  and compiler_flags=${compiler_flags}`);
+        const stmt = await this.connection.prepare(
+            `delete from latest
+                where library=@library
+                  and library_version=@library_version
+                  and compiler=@compiler
+                  and compiler_version=@compiler_version
+                  and arch=@arch
+                  and libcxx=@libcxx
+                  and compiler_flags=@compiler_flags`);
+    
+        await stmt.bind({
+            '@library': library,
+            '@library_version': library_version,
+            '@compiler': compiler,
+            '@compiler_version': compiler_version,
+            '@arch': arch,
+            '@libcxx': libcxx,
+            '@compiler_flags': compiler_flags
+        });
+
+        await stmt.run();
     }
 
     async setBuildFailed(library, library_version, compiler, compiler_version, arch, libcxx, compiler_flags, logging) {
         const now = this.getCurrentDateStr();
-        const query = SQL`replace into latest
+
+        const stmt = await this.connection.prepare(
+            `replace into latest
             ( library, library_version, compiler, compiler_version, arch, libcxx, compiler_flags, success, build_dt, logging)
             values
-            ( ${library}, ${library_version}, ${compiler}, ${compiler_version}, ${arch}, ${libcxx}, ${compiler_flags}, 0, ${now}, ${logging});`;
+            ( @library, @library_version, @compiler, @compiler_version, @arch, @libcxx, @compiler_flags, 0, @now, @logging);`);
 
-        await this.connection.exec(query);
+        await stmt.bind({
+            '@library': library,
+            '@library_version': library_version,
+            '@compiler': compiler,
+            '@compiler_version': compiler_version,
+            '@arch': arch,
+            '@libcxx': libcxx,
+            '@compiler_flags': compiler_flags,
+            '@now': now,
+            '@logging': logging
+        });
+
+        await stmt.run();
     }
 
     async listBuilds() {
@@ -55,16 +84,29 @@ class BuildLogging {
         return results;
     }
 
-    async getLogging(library, library_version, compiler, compiler_version, compiler_libcxx, compiler_flags) {
-        const results = await this.connection.all(
-            SQL`select logging
+    async getLogging(library, library_version, compiler, compiler_version, libcxx, compiler_flags) {
+        const stmt = await this.connection.prepare(
+            `select logging
                  from latest
-                where library=${library}
-                  and library_version=${library_version}
-                  and compiler=${compiler}
-                  and compiler_version=${compiler_version}
-                  and compiler_libcxx=${compiler_libcxx}
-                  and compiler_flags=${compiler_flags}`);
+                where library=@library
+                  and library_version=@library_version
+                  and compiler=@compiler
+                  and compiler_version=@compiler_version
+                  and compiler_libcxx=@compiler_libcxx
+                  and compiler_flags=@compiler_flags`
+        );
+
+        await stmt.bind({
+            '@library': library,
+            '@library_version': library_version,
+            '@compiler': compiler,
+            '@compiler_version': compiler_version,
+            '@arch': arch,
+            '@libcxx': libcxx,
+            '@compiler_flags': compiler_flags
+        });
+
+        const results = await stmt.all();
 
         return results;
     }
